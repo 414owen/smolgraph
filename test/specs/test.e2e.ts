b4aed5052b1ffa0e59fa0de465c6663836d9eb55
Owen Shepherd <owen@owen.cafe>
2025-08-20 23:25:10 +0100

    Add a web test (WIP)

---
+++ test/specs/test.e2e.ts
@@ -0,0 +1,19 @@
+import { expect, browser, $ } from '@wdio/globals'
+
+describe('Graph', () => {
+    it('Should be zoomable', async () => {
+        await browser.url(`http://localhost:8001/noise.html`);
+
+        const overlay = await $('.overlay');
+
+        await browser.action('wheel').scroll({
+            origin: overlay,
+            deltaX: 0,
+            deltaY: 500,
+            duration: 1000
+        }).perform();
+
+        await browser.waitUntil(() => false, {timeout: 100000000});
+    })
+})
+
